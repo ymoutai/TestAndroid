60a68a0bc4138f6334afbd4b77b5ae6bf1a74d2f
ymoutai <ymoutai1027@gmail.com>
2020-06-28 04:27:57 +0900

    じゃんけんアプリ動作可能 【変更箇所】MainActivity.java RpsActivity.java activity_main.xml result_rps.xml AndroidManifest.xml 【変更理由】じゃんけんアプリが一旦動作可能になったため

--- app/src/main/java/com/example/testandroid/MainActivity.java
+++ app/src/main/java/com/example/testandroid/MainActivity.java
@@ -2,7 +2,13 @@ package com.example.testandroid;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Application;
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +16,35 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        // ブランクを切って再度コメントを編集したよ
+
+        /**
+         * rock     = ぐー
+         * scissors = ちょき
+         * paper    = ぱー
+         */
+        Button rockButton = findViewById(R.id.rock);
+        rockButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplication(), RpsResult.class);
+                intent.putExtra("rsp", 0);
+                startActivity(intent);
+            }
+        });
+        Button scissorsButton = findViewById(R.id.scissors);
+        scissorsButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplication(), RpsResult.class);
+                intent.putExtra("rsp", 1);
+                startActivity(intent);
+            }
+        });
+        Button paperButton = findViewById(R.id.paper);
+        paperButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplication(), RpsResult.class);
+                intent.putExtra("rsp", 2);
+                startActivity(intent);
+            }
+        });
     }
 }
